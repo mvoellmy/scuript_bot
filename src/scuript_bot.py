500c01d240ec9935b4e62927b585f987147e11d4
mvoellmy <rudi.carfell@gmail.com>
2016-01-02 16:57:25 +0100

    added welcome message

--- src/scuript_bot.py
+++ src/scuript_bot.py
@@ -11,6 +11,11 @@ def on_message(message):
         client.send_message(message.channel, 'SCURIPT BOT VERSION 0.1!')
 
 @client.event
+def on_member_join(member):
+    server = member.server
+    client.send_message(server, 'Welcome {0} to {1.name}!'.format(member.mention(), server))
+
+@client.event
 def on_ready():
     print('Logged in as')
     print(client.user.name)
