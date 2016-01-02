501d5963eb4e1b2ec9d102513dd4d526a2492755
ilakarsu <yilmaz.akarsu.ch@gmail.com>
2016-01-02 17:31:37 +0100

    + added string compare for exact match

--- src/scuript_bot.py
+++ src/scuript_bot.py
@@ -5,9 +5,10 @@ client.login('miro@voellmy.com', 'beep-beep')
 
 @client.event
 def on_message(message):
-    if message.content.startswith('!hello'):
+    if message == '!hello':
         client.send_message(message.channel, 'Hello was received!')
-    if message.content.startswith('!version'):
+
+    if message == '!version':
         client.send_message(message.channel, 'SCURIPT BOT VERSION 0.1!')
 
 @client.event
@@ -22,5 +23,6 @@ def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
 
 client.run()
