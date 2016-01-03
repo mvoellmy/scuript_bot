e5ffda54483979afd2546f5d0cd3cc499a895e3c
ilakarsu <yilmaz.akarsu.ch@gmail.com>
2016-01-03 01:23:49 +0100

    + formatting

--- src/league_functions.py
+++ src/league_functions.py
@@ -6,11 +6,11 @@ w = RiotWatcher('b6e57fc8-b03e-40ce-8c84-55d616941248', default_region=EUROPE_WE
 
 
 queue_types = {
-    0 : 'CUSTOM',  # Custom games
-    2 : 'NORMAL 5x5 BLIND',  # Normal 5v5 blind pick
-    8 : 'NORMAL 3x3',  # Normal 3v3 games
+    0  : 'CUSTOM',  # Custom games
+    2  : 'NORMAL 5x5 BLIND',  # Normal 5v5 blind pick
+    8  : 'NORMAL 3x3',  # Normal 3v3 games
     14 : 'NORMAL 5x5 DRAFT',  # Normal 5v5 Draft Pick games
-    4 : 'RANKED SOLO 5x5',  # Ranked Solo 5v5 games
+    4  : 'RANKED SOLO 5x5',  # Ranked Solo 5v5 games
     41 : 'RANKED TEAM 3x3',  # Ranked Team 3v3 games
     42 : 'RANKED TEAM 5x5',  # Ranked Team 5v5 games
     65 : 'ARAM 5x5',  # ARAM games
