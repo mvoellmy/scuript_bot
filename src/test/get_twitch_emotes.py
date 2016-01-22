3d8c5e6a8b8bccf22aa2d8ed74c4394a0d336725
mvoellmy <rudi.carfell@gmail.com>
2016-01-22 13:16:30 +0100

    twitch_emotes struggle

--- src/test/get_twitch_emotes.py
+++ src/test/get_twitch_emotes.py
@@ -1,20 +1,5 @@
-'''from urllib.request import urlopen
-import os
-import json
-
-if not os.path.exists('../../images/emotes'):
-    os.makedirs('../../images/emotes')
-print('Saving emotes to folder: ' + os.path.abspath('./emotes') + '...')
-print('Grabbing emote list...')
-emotes = json.load(urlopen('https://twitchemotes.com/api_cache/v2/global.json'))
-for code, emote in emotes['emotes'].items():
-    print('Downloading: ' + code + '...')
-    urllib.urlretrieve('http:' + emotes['template']['large'].replace('{image_id}', str(emote['image_id'])),
-                       './emotes/' + code + '.png')
-print('Done! Kappa')'''
-
-from urllib.request import urlopen
 import urllib
+import requests
 import os
 import json
 
@@ -22,11 +7,15 @@ if not os.path.exists('./emotes'):
     os.makedirs('./emotes')
 print('Saving emotes to folder: ' + os.path.abspath('./emotes') + '...')
 print('Grabbing emote list...')
-response = urlopen('https://twitchemotes.com/api_cache/v2/global.json')
-response = urllib.parse.rlencode(response)
-emotes = json.load(response)
-for code, emote in emotes['emotes'].items():
-    print('Downloading: ' + code + '...')
-    urllib.urlretrieve('http:' + emotes['template']['large'].replace('{image_id}', str(emote['image_id'])),
-                       './emotes/' + code + '.png')
+
+# Why does this not work?
+response = requests.get('https://twitchemotes.com/api_cache/v2/global.json')
+response.status_code
+
+#emotes = json.load(urllib.urlopen('https://twitchemotes.com/api_cache/v2/global.json'))
+#for code, emote in emotes['emotes'].items():
+#    print('Downloading: ' + code + '...')
+#    urllib.urlretrieve('http:' + emotes['template']['large'].replace('{image_id}', str(emote['image_id'])),
+#                       './emotes/' + code + '.png')
+
 print('Done! Kappa')
